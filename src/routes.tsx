ce60dd6230744f304616e5291634dcc27044ee9f
Shuashuaa <jovijoshuatania027@gmail.com>
2025-04-04 22:13:23 +0800

    simplified the routes for countries and it's children

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router";
-import { Suspense } from "react";
 import Layout from "./pages/Layout";
 import App from "./pages/App";
 import About from "./pages/About";
@@ -10,20 +9,24 @@ import Country, { CountryError, countryLoader } from "./pages/Country";
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Layout, // Layout wraps routes with Navbar/Footer
+    Component: Layout,
     children: [
       { index: true, Component: App },
       { path: "about", Component: About },
       {
         path: "countries",
         children: [
-          { index: true, loader: countriesLoader, Component: () => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Countries />
-            </Suspense>
-          ) },
-          { path: ":countryName", loader: countryLoader, Component: Country, errorElement: <CountryError /> },
-          // { path: "trending", Component: <ConcertsTrending /> },
+          { 
+            index: true, loader: 
+            countriesLoader, 
+            Component: Countries 
+          },
+          { 
+            path: ":countryName", 
+            loader: countryLoader, 
+            Component: Country, 
+            errorElement: <CountryError /> 
+          },
         ]
       },
     ],
